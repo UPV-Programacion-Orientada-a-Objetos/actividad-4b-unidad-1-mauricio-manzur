0e889b3cfd4f9335f76ae3d6cc2ee68cdac13c69
Manzur <rfarfa965@gmail.com>
2025-10-16 15:01:54 -0600

    Se definieron los metodos de la clase Simulador2D en su archivo respectivo, contiene la definicion del constructor que crea la matriz, el destructor que libera memoria, redimension y agregar fuentes, también se implementó un menú principal para la elección de opciones para elegir (aun no tiene funcionalidad terminada).

--- Simulador2D.h
+++ Simulador2D.h
@@ -2,12 +2,12 @@ template <typename T>
 class Simulador2D {
 private:
     // Matriz de la cuadrícula (Dinámica y Genérica)
-    T **_grid;
+    T **_grid = nullptr;
     int _filas;
     int _columnas;
 
     // Vector de Fuentes (Dinámico y Genérico)
-    T *_fuentes;
+    T *_fuentes = nullptr;
     int _numFuentes;
     int _capacidadFuentes;
 
@@ -16,10 +16,150 @@ private:
 
 public:
     // MÉTODOS PÚBLICOS OBLIGATORIOS:
-    Simulador2D(int f, int c); // Constructor
-    ~Simulador2D(); // Destructor
-    void redimensionarGrid(int nuevaF, int nuevaC);
-    void agregarFuente(T valor); // Expansión manual del vector dinámico si es necesario
-    void simularPaso(); // Implementación del algoritmo
-    // ... otros métodos de acceso y visualización
+    Simulador2D(int f, int c){
+
+        // crear grid nuevo
+        _grid = new T*[f];
+
+        for(int i = 0; i < f; i++){
+
+            _grid[i] = new T[c];
+
+        }   
+
+        for(int i = 0; i < f; i++){
+
+            for(int j = 0; j < c; j++){
+
+                _grid[i][j] = 0;
+
+            }
+
+        }
+
+        _filas = f;
+        _columnas = c;
+
+    } // Constructor
+
+    ~Simulador2D(){
+
+        std::cout << "\nLiberando memoria de Grid y Fuentes...";
+
+        // elimino la matriz de la memoria cuando se destruya el objeto.
+        for(int i = 0; i < _filas; i++){
+
+            delete[] _grid[i]; 
+
+        }
+
+        delete[] _grid;
+
+        // elimino el vector dinámico de la memoria
+        delete[] _fuentes;
+
+        std::cout << " Sistema Liberado." << std::endl;
+
+    } // Destructor
+
+    void redimensionarGrid(int nuevaF, int nuevaC){
+
+        T **nuevaMat = new T*[nuevaF];
+
+        for(int i = 0; i < nuevaF; i++){
+
+            nuevaMat[i] = new T[nuevaC];
+
+        }
+
+        // copiar todos los elementos del vector anterior y pasarlos al nuevo vector Columna + 1
+        for(int i = 0; i < nuevaF; i++){
+
+            for(int j = 0; j < nuevaC; j++){
+
+                nuevaMat[i][j] = _grid[i][j];
+
+            }
+
+        }
+
+        _grid = nuevaMat;
+
+        // eliminar el contenido de las filas de la matriz.
+        for(int i = 0; i < nuevaF; i++){
+
+            delete[] nuevaMat[i];
+
+        }
+
+        // eliminar el vector principal
+        delete[] nuevaMat;
+
+        nuevaMat = nullptr;
+
+        _filas = nuevaF;
+        _columnas = nuevaC;
+
+        std::cout << "Se añadió correctamente la nueva dimension de la matriz" << std::endl;
+
+    }
+
+    // metodo para agregar una fuente al vector dinamico ( si numero de fuentes excede la capacidad del vector, agregar uno nuevo.)
+    void agregarFuente(T valor){
+
+        if(_numFuentes >= _capacidadFuentes){
+
+            T *nuevaFuentes = new T[_capacidadFuentes+1];
+
+            for(int i = 0; i < _capacidadFuentes; i++){
+
+                nuevaFuentes[i] = _fuentes[i];
+            
+            }
+
+            _fuentes = nuevaFuentes;
+            delete[] nuevaFuentes;
+            nuevaFuentes = nullptr;
+
+            _capacidadFuentes++;
+
+        }
+
+        _fuentes[_capacidadFuentes] = valor;
+        _numFuentes++;
+
+        std::cout << "Se agregó la fuente correctamente." << std::endl;
+
+    } // Expansión manual del vector dinámico si es necesario
+
+    void simularPaso(){
+
+
+
+    } // Implementación del algoritmo
+
+
+    // metodo que imprimie el grid en pantalla
+    void mostrarGrid(){
+
+        std::cout << "---- Visualizar Grid ----" << std::endl;
+
+        for(int i = 0; i < _filas; i++){
+
+            std::cout << "| ";
+
+            for(int j = 0; j < _columnas; j++){
+
+                std::cout << " " << _grid[i][j] << " |";
+
+            }
+
+            std::cout << std::endl;
+
+        }
+
+    }
+
+
+
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,65 @@
 #include <iostream>
+#include "Simulador2D.h"
 
-int main() {
+void inicio();
+
+int main(){
+
+    inicio();
+
+
+
+
+
     return 0;
+
+}
+
+
+void inicio(){
+
+    do{
+
+        int opcion = 0;
+
+        std::cout << "---- Modelado de Propagación Física Manzur ----" << std::endl;
+        std::cout << "-----------------------------------------------" << std::endl;
+        std::cout << "[1] Construir una nueva Matriz" << std::endl;
+        std::cout << "[2] Aplicar Valores a la Matriz (Por fila)" << std::endl;
+        std::cout << "[3] Simular paso siguiente" << std::endl;
+        std::cout << "[4] Finalizar Sistema" << std::endl;
+        std::cout << "-----------------------------------------------" << std::endl;
+        std::cout << "\n" << std::endl;
+
+        std::cin.ignore();
+        std::cin >> opcion;
+
+        if(std::cin.good()){
+
+            switch (opcion)
+            {
+            case 1:
+                /* code */
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                std::cout << "\n--- GRACIAS POR UTILIZAR LA APLICACIÓN, FINALIZANDO SISTEMA ---" << std::endl;
+                break;
+
+            default:
+                std::cout << "\n--- INGRESE UNA OPCIÓN VÁLIDA ---" << std::endl;
+                break;
+            }
+
+        }
+
+
+    }while(true);
+
 }
